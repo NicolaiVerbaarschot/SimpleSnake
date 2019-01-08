3e6b76502d872d0ea7c5f5229bafc0cc96a17932
AGRossau <zaikaze@gmail.com>
2019-01-08 10:28:17 +0100

    Implemented Snake class

--- src/Model/Snake.java
+++ src/Model/Snake.java
@@ -1,4 +1,22 @@
 package Model;
 
+import java.awt.Point;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Snake {
+    private List<Point> snake_locations = new ArrayList<>();
+
+    public Snake(int grid_x, int grid_y) {
+        snake_locations.add(0, new Point(grid_x/2, grid_y/2));
+        snake_locations.add(0, new Point(grid_x/2, grid_y/2 + 1));
+    }
+
+    public void move(int x, int y, boolean will_grow) {
+        snake_locations.add(0, new Point(x, y));
+
+        if (!will_grow) {
+            snake_locations.remove(snake_locations.size() - 1);
+        }
+    }
 }
