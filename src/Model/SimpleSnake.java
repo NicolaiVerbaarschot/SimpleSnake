84a1e027376e86caa713631ac9e1a6951a0609f6
BirkBerger <s163703@student.dtu.dk>
2019-01-08 16:45:50 +0100

    Created wall collision method for SimpleSnake

--- src/Model/SimpleSnake.java
+++ src/Model/SimpleSnake.java
@@ -10,6 +10,8 @@ public class SimpleSnake {
     private Snake solid;
     private Mouse mickey;
     private Gameboard gameboard;
+    private int grid_x;
+    private int grid_y;
 
     /**
      * Constructor
@@ -19,6 +21,9 @@ public class SimpleSnake {
      */
     public SimpleSnake(int grid_x, int grid_y) {
 
+        // Set gameboard size
+        this.grid_x = grid_x;
+        this.grid_y = grid_y;
         // Create snake
         this.solid = new Snake(grid_x, grid_y);
         // Create gameboard
@@ -34,62 +39,79 @@ public class SimpleSnake {
     /**
      * gameAction
      * @param key_input
-     * @param grid_x
-     * @param grid_y
      * @author Thea Birk Berger
      */
-    public void gameAction(String key_input, int grid_x, int grid_y) {
+    public void gameAction(String key_input) {
 
         List<Point> snake_location = solid.get_location();
-        Point newHead = snake_location.get(snake_location.size() - 1);
+        Point new_head = snake_location.get(snake_location.size() - 1);
 
-        // Determine newHead coordinates
+        // Determine new_head coordinates
         switch (key_input) {
             case "up":
-                newHead.setLocation(newHead.getX(), newHead.getY() - 1);
+                new_head.setLocation(new_head.getX(), new_head.getY() - 1);
                 break;
             case "down":
-                newHead.setLocation(newHead.getX(), newHead.getY() + 1);
+                new_head.setLocation(new_head.getX(), new_head.getY() + 1);
                 break;
             case "left":
-                newHead.setLocation(newHead.getX() - 1, newHead.getY());
+                new_head.setLocation(new_head.getX() - 1, new_head.getY());
                 break;
             case "right":
-                newHead.setLocation(newHead.getX() + 1, newHead.getY());
+                new_head.setLocation(new_head.getX() + 1, new_head.getY());
                 break;
             default:
                 break;
         }
 
-        // Check for collision
-        for (int i = 0; i < snake_location.size(); i++) {
-            if (snake_location.get(i) == newHead.getLocation()) {
-                // TODO: implement gameOver() as a method in SimpleSnake;
-            }
+        // Updating new_head according wall collision
+        new_head.x = wall_collision_check((int) new_head.getX(), grid_x-1);
+        new_head.y = wall_collision_check((int) new_head.getY(), grid_y-1);
+
+        // Check for snake collision
+        if (snake_location.contains(new_head)) {
+            // TODO: implement gameOver() as a method in SimpleSnake;
         }
 
         // Check for mouse
-        if (newHead.getX() == mickey.get_x_coordinate() && newHead.getY() == mickey.get_y_coordinate()) {
+        if (new_head.getX() == mickey.get_x_coordinate() && new_head.getY() == mickey.get_y_coordinate()) {
             // Grow snake
-            solid.move((int) newHead.getX(), (int) newHead.getY(), true);
+            solid.move((int) new_head.getX(), (int) new_head.getY(), true);
             // Update map
-            gameboard.remove(newHead);
+            gameboard.remove(new_head);
             // Update mouse location
             mickey.update_location(gameboard.get_board());
         }
 
         // Check for impossible action - otherwise move snake
-        else if (newHead.getLocation() != snake_location.get(snake_location.size() - 2)) {
+        else if (new_head.getLocation() != snake_location.get(snake_location.size() - 2)) {
             // Move snake and define tail
-            Point tail = solid.move((int) newHead.getX(), (int) newHead.getY(), false);
+            Point tail = solid.move((int) new_head.getX(), (int) new_head.getY(), false);
             // Update map
-            gameboard.remove(newHead);
+            gameboard.remove(new_head);
             gameboard.add(tail);
         }
     }
 
     public List<Point> get_snake_location() {
         return solid.get_location();
+    }
+
+
+    /**
+     * @param coordinate
+     * @param coordinate_max
+     * @return
+     * @author Andreas Goll Rossau
+     */
+    private int wall_collision_check(int coordinate, int coordinate_max) {
+        if(coordinate == -1) {
+            coordinate += coordinate_max;
+        }
+        else if (coordinate == coordinate_max){
+            coordinate = 0;
+        }
+        return coordinate;
     }
 }
 
