287157e9411e1ea05cb13b00116e3d2b96abcb12
BirkBerger <s163703@student.dtu.dk>
2019-01-10 10:40:25 +0100

    Added "key input translation switch" to gameAction instead of having a separate "create new head"-method

--- src/Model/SimpleSnake.java
+++ src/Model/SimpleSnake.java
@@ -46,42 +46,6 @@ public class SimpleSnake {
 
         // Extracting current snake information and calculating new_head ("målfeltet")
         List<Point> snake_location = solid.get_location();
-        Point new_head = creating_new_head(key_input, snake_location);
-
-        // Updating new_head coordinates in the event of wall collision
-        new_head.x = wall_collision_check((int) new_head.getX(), grid_x-1);
-        new_head.y = wall_collision_check((int) new_head.getY(), grid_y-1);
-
-        // Ending game in the event of snake collision
-        if (snake_location.contains(new_head) && !(new_head.getLocation() == snake_location.get(snake_location.size() - 2))) {
-            // TODO: implement gameOver() as a method in SimpleSnake;
-            // This should return game over, but at this point the game always game overs, so it is disabled for now
-            return "Game Over";
-        }
-
-        // Updating fields in the event of mouse presence
-        if (new_head.getX() == mickey.get_x_coordinate() && new_head.getY() == mickey.get_y_coordinate()) {
-            grow_snake(new_head, snake_location);
-        }
-
-        // Updating fields in the event of no opposite direction attempt
-        else if (!(new_head.getLocation() == snake_location.get(snake_location.size() - 2))) {
-            move_snake(new_head);
-        }
-
-        return "Ok";
-    }
-
-
-    /**
-     * creating_new_head
-     * @param key_input
-     * @param snake_location
-     * @author Thea Birk Berger
-     */
-    // Translate key input to new_head coordinates
-    private Point creating_new_head(String key_input, List<Point> snake_location) {
-        // Extract current snake head information
         Point new_head = new Point(snake_location.get(0));
 
         switch (key_input) {
@@ -93,17 +57,60 @@ public class SimpleSnake {
                 new_head.setLocation(new_head.getX() - 1, new_head.getY()); break;
             case "right":
                 new_head.setLocation(new_head.getX() + 1, new_head.getY()); break;
-            default: break;
+            default: return "Ok";
         }
-        return new_head;
-    }
 
+        // Updating new_head coordinates in the event of wall collision
+        new_head.x = wall_collision_check((int) new_head.getX(), grid_x-1);
+        new_head.y = wall_collision_check((int) new_head.getY(), grid_y-1);
 
-        // Check for snake collision
+        // Ending game in the event of snake collision
         if (snake_location.contains(new_head) && !new_head.equals(snake_location.get(1))) {
             // TODO: implement gameOver() as a method in SimpleSnake;
             // This should return game over, but at this point the game always game overs, so it is disabled for now
             return "Game Over";
+        }
+
+        // Updating fields in the event of mouse presence
+        if (new_head.getX() == mickey.get_x_coordinate() && new_head.getY() == mickey.get_y_coordinate()) {
+            grow_snake(new_head);
+        }
+
+        // Updating fields in the event of no opposite direction attempt
+        else if (!new_head.getLocation().equals(snake_location.get(1))) {
+            move_snake(new_head);
+        }
+
+        return "Ok";
+    }
+
+//
+//    /**
+//     * creating_new_head
+//     * @param key_input
+//     * @param snake_location
+//     * @author Thea Birk Berger
+//     */
+//    // Translate key input to new_head coordinates
+//    private Point creating_new_head(String key_input, List<Point> snake_location) {
+//        // Extract current snake head information
+//        Point new_head = new Point(snake_location.get(0));
+//
+//        switch (key_input) {
+//            case "up":
+//                new_head.setLocation(new_head.getX(), new_head.getY() - 1); break;
+//            case "down":
+//                new_head.setLocation(new_head.getX(), new_head.getY() + 1); break;
+//            case "left":
+//                new_head.setLocation(new_head.getX() - 1, new_head.getY()); break;
+//            case "right":
+//                new_head.setLocation(new_head.getX() + 1, new_head.getY()); break;
+//            default: break;
+//        }
+//        return new_head;
+//    }
+
+
     /**
      * wall_collision_check
      * @param coordinate
@@ -115,7 +122,7 @@ public class SimpleSnake {
         if(coordinate == -1) {
             coordinate = coordinate_max;
         }
-        else if (coordinate == coordinate_max){
+        else if (coordinate == coordinate_max + 1){
             coordinate = 0;
         }
         return coordinate;
@@ -125,10 +132,9 @@ public class SimpleSnake {
     /**
      * grow_snake
      * @param new_head
-     * @param snake_location
      * @author Thea Birk Berger
      */
-    private void grow_snake(Point new_head, List<Point> snake_location) {
+    private void grow_snake(Point new_head) {
         // Grow snake
         solid.move((int) new_head.getX(), (int) new_head.getY(), true);
         // Update map
