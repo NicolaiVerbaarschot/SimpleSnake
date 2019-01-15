bcb4956b9f285a14e91bf0eeb4f523b668219b4e
NicolaiV <nicolai.verbaarschot@gmail.com>
2019-01-15 22:25:58 +0100

    Controller package refactored.

--- src/Controller/SimpleSnakeController.java
+++ src/Controller/SimpleSnakeController.java
@@ -2,34 +2,34 @@ package Controller;
 
 import Model.SimpleSnake;
 import View.SimpleSnakeView;
-import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
- * Class controls the Simple Snake game
+ * This Class controls the Simple Snake game
+ *
  * @author Andreas Goll Rossau
  */
 public class SimpleSnakeController {
+
     private SimpleSnake game;
-    // SnakeTextViewKeyPress view;
     private SimpleSnakeView view;
-    private  String game_status;
-    private int grid_x;
-    private int grid_y;
     private boolean endgame_flag;
 
     /**
      * Constructor. The program never leaves this constructor unless the game ends
-     * @param grid_x The grid size in the x dimension
-     * @param grid_y The grid size in the y dimension
+     *
+     * @param grid_x        The grid size in the x dimension
+     * @param grid_y        The grid size in the y dimension
+     * @param gridPane      JavaFX Node
+     * @param primary_stage JavaFX Node
      * @author Andreas Goll Rossau
      */
     public SimpleSnakeController(int grid_x, int grid_y, GridPane gridPane, Stage primary_stage) {
+
         this.game = new SimpleSnake(grid_x, grid_y);
         this.view = new SimpleSnakeView(grid_x, grid_y, gridPane, primary_stage);
-        this.grid_x = grid_x;
-        this.grid_y = grid_y;
+
         endgame_flag = false;
 
         // Initialize window
@@ -39,33 +39,35 @@ public class SimpleSnakeController {
 
     /**
      * Method passes key input code to model and updates view according to game status returned from model
+     *
      * @param code key input code
-     * @author
+     * @author Andread Goll Rossau
      */
     public void key_press(String code) {
-        if (endgame_flag && !(code.equals("R") || code.equals("ESCAPE"))) {
-            return;
-        }
-        game_status = game.game_action(code);
 
-        if (game_status.equals("Playing")) {
-            view.update_board(game.get_snake_location().get(0), game.get_tail(), game.get_mouse_location());
-            view.set_score_bar(game.get_score());
-        }
-        else if (game_status.equals("Restart")) {
-            game.reset_game();
-            view.clear_endgame();
-            view.draw_board(game.get_snake_location(), game.get_mouse_location());
-            view.set_score_bar(game.get_score());
-            view.set_score_bar(game.get_score());
-            endgame_flag = false;
-        }
-        else if (game_status.equals("Exit")){
-            System.exit(0);
-        }
-        else {
-            view.print_status(game_status);
-            endgame_flag = true;
+        if (endgame_flag && !(code.equals("R") || code.equals("ESCAPE")))
+            return;
+
+        String game_status = game.game_action(code);
+        switch (game_status) {
+            case "Playing":
+                view.update_board(game.get_snake_location().get(0), game.get_tail(), game.get_mouse_location());
+                view.set_score_bar(game.get_score());
+                break;
+            case "Restart":
+                game.reset_game();
+                view.clear_endgame();
+                view.draw_board(game.get_snake_location(), game.get_mouse_location());
+                view.set_score_bar(game.get_score());
+                view.set_score_bar(game.get_score());
+                endgame_flag = false;
+                break;
+            case "Exit":
+                System.exit(0);
+            default:
+                view.print_status(game_status);
+                endgame_flag = true;
+                break;
         }
     }
 }
--- src/Controller/SnakeTextController.java
+++ src/Controller/SnakeTextController.java
@@ -4,30 +4,32 @@ import Model.SimpleSnake;
 import View.SnakeTextView;
 
 /**
- * Class controls the Simple Snake game
+ * This Class controls the Simple Snake game
+ *
  * @author Andreas Goll Rossau
  */
 public class SnakeTextController {
-    SimpleSnake game;
-    SnakeTextView view;
-    String game_status;
 
     /**
      * Constructor. The program never leaves this constructor unless the game ends
+     *
      * @param grid_x The grid size in the x dimension
      * @param grid_y The grid size in the y dimension
      * @author Andreas Goll Rossau
      */
     public SnakeTextController(int grid_x, int grid_y) {
-        this.game = new SimpleSnake(grid_x, grid_y);
-        this.view = new SnakeTextView();
+
+        SimpleSnake game = new SimpleSnake(grid_x, grid_y);
+        SnakeTextView view = new SnakeTextView();
 
         // This is the game loop only exited on game over
-        do {
+        String game_status;
+        do
+        {
             view.drawBoard(grid_x, grid_y, game.get_snake_location(), game.get_mouse_location(), game.get_score());
-            this.game_status = game.game_action(view.getInput());
+            game_status = game.game_action(view.getInput());
         }
-        while(game_status.equals("Playing"));
+        while (game_status.equals("Playing"));
 
         System.out.println(game_status);
 
