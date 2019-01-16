7e508375fe735efbd66e228ca49b967b4c99c44b
NicolaiV <nicolai.verbaarschot@gmail.com>
2019-01-16 14:26:10 +0100

    Added SnakeSegment Class; containing the current Point, but also the next and previous Points. Snake class no longer contains list of points but list of SnakeSegments.

--- src/Model/Snake.java
+++ src/Model/Snake.java
@@ -11,7 +11,8 @@ import java.util.ArrayList;
  */
 class Snake {
 
-    private List<Point> snake_location = new ArrayList<>();
+//    private List<Point> snake_location = new ArrayList<>();
+    private List<SnakeSegment> snake = new ArrayList<>();
 
     /** Snake constructor determines the initial position of the snake from the size of the grid
      *
@@ -20,8 +21,13 @@ class Snake {
      * @author  Andreas Goll Rossau
      */
     Snake(int grid_x, int grid_y) {
-        snake_location.add(0, new Point(grid_x/2, grid_y/2));
-        snake_location.add(0, new Point(grid_x/2, grid_y/2 + 1));
+//        snake_location.add(0, new Point(grid_x/2, grid_y/2));
+//        snake_location.add(0, new Point(grid_x/2, grid_y/2 + 1));
+
+        Point initial_head = new Point(grid_x/2, grid_y/2 - 1);
+        Point initial_tail = new Point(grid_x/2, grid_y/2);
+        snake.add(0, new SnakeSegment(initial_tail, null, null));
+        snake.add(0, new SnakeSegment(initial_head, null, null));
     }
 
     /**
@@ -35,16 +41,16 @@ class Snake {
      */
     Point move(int x, int y, boolean will_grow) {
 
-        Point tail = new Point();
+        SnakeSegment tail = new SnakeSegment();
 
-        snake_location.add(0, new Point(x, y));
+        snake.add(0, new SnakeSegment(new Point(x, y), null, null));
 
         // If the snake does not grow, the tail is deleted
         if (!will_grow) {
-            tail = snake_location.get(snake_location.size() - 1);
-            snake_location.remove(snake_location.size() - 1);
+            tail = snake.get(snake.size() - 1);
+            snake.remove(snake.size() - 1);
         }
-        return tail;
+        return tail.get_coordinates();
     }
 
     /**
@@ -54,7 +60,16 @@ class Snake {
      * @author Andreas Goll Rossau
      */
     List<Point> get_location() {
-        return new ArrayList<>(snake_location);
+        List<Point> points = new ArrayList<>();
+
+        for (int i=snake.size()-1; i>=0; i--) {
+            points.add(0,snake.get(i).get_coordinates());
+        }
+        return points;
     }
+
+
+
+
 
 }
---
+++ src/Model/SnakeSegment.java
@@ -0,0 +1,71 @@
+package Model;
+
+import java.awt.Point;
+
+public class SnakeSegment {
+
+    private boolean is_head;
+    private boolean is_tail;
+    private Point coordinates;
+    private Point previous_coordinates;
+    private Point next_coordinates;
+
+    SnakeSegment() {
+
+        this.coordinates = null;
+        this.previous_coordinates = null;
+        this.next_coordinates = null;
+    }
+
+    SnakeSegment(Point coordinates, Point previous_coordinates, Point next_coordinates) {
+
+        this.coordinates = coordinates;
+        this.previous_coordinates = previous_coordinates;
+        this.next_coordinates = next_coordinates;
+    }
+
+    public Point get_coordinates() {
+        return coordinates;
+    }
+
+    public Point get_next_coordinates() {
+        return next_coordinates;
+    }
+
+    public Point get_previous_coordinates() {
+        return previous_coordinates;
+    }
+
+    public void set_coordinates(Point coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public void set_next_coordinates(Point next_coordinates) {
+        this.next_coordinates = next_coordinates;
+    }
+
+    public void set_previous_coordinates(Point previous_coordinates) {
+        this.previous_coordinates = previous_coordinates;
+    }
+
+    public void set_head(boolean value) {
+        this.is_head = value;
+    }
+
+    public void set_tail(boolean value) {
+        this.is_tail = value;
+    }
+
+    public boolean is_head() {
+        return is_head;
+    }
+
+    public boolean is_tail() {
+        return is_tail;
+    }
+
+
+
+
+
+}
